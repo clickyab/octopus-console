3d7497edc087d45a1856cc1ce060216a9659a21e
kasra <kasra.ansari@yahoo.com>
2017-07-10 16:38:15 +0430

    add sidebar ui & route
    fix #2658

--- src/app/sciences/sidebar/index.js
+++ src/app/sciences/sidebar/index.js
@@ -1,46 +1,62 @@
 import React, {Component} from 'react';
 import {Menu, Icon} from 'antd';
 import {dispatch} from '../../services/dispatch';
-import './style.css';
 import {failedLogin} from "../../redux/actions/index";
-import { withRouter } from 'react-router-dom'
-
-const {SubMenu} = Menu;
+import {withRouter} from 'react-router-dom'
+import './style/index.less';
 
 class Sidebar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(e) {
-        if (e.key === 'logout') {
-            this.logOut()
+    handleClick = e => {
+        switch (e.key) {
+            case 'dashboard':
+                this.props.history.push('/dashboard');
+                break;
+            case 'supplier':
+                this.props.history.push('/supplier');
+                break;
+            case 'demand':
+                this.props.history.push('/demand');
+                break;
+            case 'logout':
+                this.logOut();
+                break;
+            default:
+                return;
         }
-    }
+    };
 
-    logOut() {
+    logOut = () => {
         dispatch(failedLogin());
         this.props.history.push('/login');
-    }
+    };
+
     render() {
         return (
             <div>
-                <div className="logo" />
-                <Menu theme="dark" mode={this.props.mode} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']}
-                onClick={this.handleClick}>
-                    <SubMenu
-                        key="sub1"
-                        title={<span><Icon type="user" /><span className="nav-text">Dashboard</span></span>}
-                    >
-                        <Menu.Item key="1">profile</Menu.Item>
-                        <Menu.Item key="2">demand</Menu.Item>
-                        <Menu.Item key="3">supplier</Menu.Item>
-                    </SubMenu>
+                <div className="logo"/>
+                <Menu theme="dark" mode={this.props.mode} defaultSelectedKeys={['dashboard']}
+                      onClick={this.handleClick}>
+                    <Menu.Item key="dashboard">
+                        <span>
+                            <Icon type="home"/>
+                            <span className="nav-text">Dashboard</span>
+                        </span>
+                    </Menu.Item>
+                    <Menu.Item key="supplier">
+                        <span>
+                            <Icon type="man"/>
+                            <span className="nav-text">Supplier</span>
+                        </span>
+                    </Menu.Item>
+                    <Menu.Item key="demand">
+                        <span>
+                            <Icon type="woman"/>
+                            <span className="nav-text">Demand</span>
+                        </span>
+                    </Menu.Item>
                     <Menu.Item key="logout">
                             <span>
-                                <Icon type="file" />
+                                <Icon type="logout"/>
                                 <span className="nav-text">LogOut</span>
                             </span>
                     </Menu.Item>
