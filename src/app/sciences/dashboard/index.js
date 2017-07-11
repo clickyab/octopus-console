1d2208927c63b3144ddac73166c279dbd4470f9b
kasra <kasra.ansari@yahoo.com>
2017-07-11 17:57:22 +0430

    add dashboard page

--- src/app/sciences/dashboard/index.js
+++ src/app/sciences/dashboard/index.js
@@ -1,17 +1,10 @@
 import React, {Component} from 'react';
-import {Breadcrumb} from 'antd';
 
 export default class Dashboard extends Component {
     render() {
-        return(
-            <div>
-                <Breadcrumb style={{ margin: '12px 0' }}>
-                    <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-                    <Breadcrumb.Item>profile</Breadcrumb.Item>
-                </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 500}}>
-                    Welcome to Exchange
-                </div>
+        return (
+            <div style={{padding: 24, background: '#fff', minHeight: 500}}>
+                Welcome!
             </div>
         )
     }
