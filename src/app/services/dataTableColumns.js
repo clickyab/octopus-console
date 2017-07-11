98449556e423d34a62aa6af8f5a4f1c4a20e0985
kasra <kasra.ansari@yahoo.com>
2017-07-11 15:52:22 +0430

    data table column desgin

---
+++ src/app/services/dataTableColumns.js
@@ -0,0 +1,57 @@
+export const columns = [{
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id'
+}, {
+    title: 'Demand',
+    dataIndex: 'demand',
+    key: 'demand'
+},{
+    title: 'Target date',
+    dataIndex: 'target_date',
+    key: 'target_date'
+}, {
+    title: 'Request out count',
+    dataIndex: 'request_out_count',
+    key: 'request_out_count'
+}, {
+    title: 'Ad in count',
+    dataIndex: 'ad_in_count',
+    key: 'ad_in_count'
+}, {
+    title: 'Imp out count',
+    dataIndex: 'imp_out_count',
+    key: 'imp_out_count'
+}, {
+    title: 'Ad out count',
+    dataIndex: 'ad_out_count',
+    key: 'ad_out_count'
+}, {
+    title: 'Ad out bid',
+    dataIndex: 'ad_out_bid',
+    key: 'ad_out_bid'
+}, {
+    title: 'Deliver count',
+    dataIndex: 'deliver_count',
+    key: 'deliver_count'
+}, {
+    title: 'Deliver bid',
+    dataIndex: 'deliver_bid',
+    key: 'deliver_bid'
+}, {
+    title: 'Profit',
+    dataIndex: 'profit',
+    key: 'profit'
+}, {
+    title: 'Success rate',
+    dataIndex: 'success_rate',
+    key: 'success_rate'
+}, {
+    title: 'Deliver rate',
+    dataIndex: 'deliver_rate',
+    key: 'deliver_rate'
+}, {
+    title: 'Win rate',
+    dataIndex: 'win_rate',
+    key: 'win_rate'
+}];
