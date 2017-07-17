69d41b8d08666af8af951d3f53fd5e6f409033a6
kasra <kasra.ansari@yahoo.com>
2017-07-17 19:06:40 +0430

    add exchange & supllier pagination & some feature

--- src/app/sciences/exchange/index.js
+++ src/app/sciences/exchange/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import {DatePicker, message, Table, Spin} from 'antd';
+import {DatePicker, message, Table, Spin, Pagination} from 'antd';
 import * as swagger from './../../swagger/index';
 import {connect} from "react-redux";
 import {dispatch} from "./../../services/dispatch";
 import {getExchangeData, userUnauthorized} from "../../redux/actions/index";
 import {exchangeColumns} from './exchangeDataColumns';
 import {http500Error} from "../../services/http500ErrorNotif";
+import {dfc} from "../../services/dateFormatChange";
+import moment from 'moment';
 
 const {RangePicker} = DatePicker;
 
@@ -13,25 +15,38 @@ const {RangePicker} = DatePicker;
 export default class Exchange extends Component {
     state = {
         loading: false,
-        exchangeData: null
+        exchangeData: this.props.exchangeData.data,
+        current: this.props.exchangeData.page || 1
     };
 
-    onChange = (data, dataString) => {
-        let from = dataString[0].replace(/-/g, "");
-        let to = dataString[1].replace(/-/g, "");
+    onChangeRangePicker = (data, dataString) => {
+        let from = dataString[0];
+        let to = dataString[1];
 
-        this.createExchange(from, to);
+        if (from.length === 0 || to.length === 0) return;
+
+        this.setState({current: 1}, () => {
+            this.createExchange(from, to);
+        })
+    };
+
+    onChangePagination = (page) => {
+        this.setState({current: page}, () => {
+            this.createExchange(this.props.exchangeData.from, this.props.exchangeData.to);
+        });
     };
 
     reqExchange = (from, to) => {
         let token = this.props.userData.token;
+        let option = {
+            p: this.state.current
+        };
         return new Promise((resolve, reject) => {
             new swagger.RoutesApi()
-                .reportExchangeFromToGet(from, to, token, {}, (error, body, response) => {
+                .reportExchangeFromToGet(from, to, token, option, (error, body, response) => {
                     if (!response) return reject({statusCode: 500});
                     if (response.statusCode === 200) return resolve(response.body);
                     else return reject(response)
-
                 })
         });
     };
@@ -40,7 +55,10 @@ export default class Exchange extends Component {
         try {
             this.setState({loading: true});
 
-            const data = await this.reqExchange(from, to);
+            const data = await this.reqExchange(dfc(from), dfc(to));
+            data.from = from;
+            data.to = to;
+            data.page = this.state.current;
             dispatch(getExchangeData(data));
 
             this.setState({exchangeData: this.props.exchangeData.data, loading: false});
@@ -58,18 +76,27 @@ export default class Exchange extends Component {
         }
     };
 
-
     disabledStartDate = (current) => {
         return current && current.valueOf() > Date.now()
     };
 
     render() {
+        let rangeDefaultVal = () => {
+            let from = this.props.exchangeData.from !== undefined ? moment(this.props.exchangeData.from) : null;
+            let to = this.props.exchangeData.to !== undefined ? moment(this.props.exchangeData.to) : null;
+
+            return [from, to];
+        };
         return (
             <div style={{padding: 24, background: '#fff', minHeight: 500}}>
                 <RangePicker style={{marginBottom: 10}} disabledDate={this.disabledStartDate} size="large"
-                             onChange={this.onChange}/>
+                             onChange={this.onChangeRangePicker}
+                             defaultValue={rangeDefaultVal()}/>
                 <Spin spinning={this.state.loading}>
-                    <Table rowKey={record => record.id} dataSource={this.state.exchangeData} columns={exchangeColumns}/>
+                    <Table rowKey={record => record.id} dataSource={this.state.exchangeData}
+                           onChange={this.onChangePagination} columns={exchangeColumns} pagination={false}/>
+                    <Pagination className="table-pagination-custom" onChange={this.onChangePagination}
+                                current={this.state.current} total={this.props.exchangeData.count}/>
                 </Spin>
             </div>
         )
--- src/app/sciences/supplier/index.js
+++ src/app/sciences/supplier/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import {DatePicker, message, Table, Spin} from 'antd';
+import {DatePicker, message, Table, Spin, Pagination} from 'antd';
 import * as swagger from './../../swagger/index';
 import {connect} from "react-redux";
 import {dispatch} from "./../../services/dispatch";
 import {getSupplierData, userUnauthorized} from "../../redux/actions/index";
-import {supplierColumns} from './supplierDataColumns';
 import {http500Error} from "../../services/http500ErrorNotif";
+import {dfc} from "../../services/dateFormatChange";
+import moment from 'moment';
+import {supplierColumns} from "./supplierDataColumns";
 
 const {RangePicker} = DatePicker;
 
@@ -13,25 +15,38 @@ const {RangePicker} = DatePicker;
 export default class Supplier extends Component {
     state = {
         loading: false,
-        supplierData: null
+        supplierData: this.props.supplierData.data,
+        current: this.props.supplierData.page || 1
     };
 
-    onChange = (data, dataString) => {
-        let from = dataString[0].replace(/-/g, "");
-        let to = dataString[1].replace(/-/g, "");
+    onChangeRangePicker = (data, dataString) => {
+        let from = dataString[0];
+        let to = dataString[1];
 
-        this.createSupplier(from, to);
+        if (from.length === 0 || to.length === 0) return;
+
+        this.setState({current: 1}, () => {
+            this.createSupplier(from, to);
+        })
+    };
+
+    onChangePagination = (page) => {
+        this.setState({current: page}, () => {
+            this.createSupplier(this.props.supplierData.from, this.props.supplierData.to);
+        });
     };
 
     reqSupplier = (from, to) => {
         let token = this.props.userData.token;
+        let option = {
+            p: this.state.current
+        };
         return new Promise((resolve, reject) => {
             new swagger.RoutesApi()
-                .reportSupplierFromToGet(from, to, token, {}, (error, body, response) => {
+                .reportSupplierFromToGet(from, to, token, option, (error, body, response) => {
                     if (!response) return reject({statusCode: 500});
-                    if (response.statusCode === 200) return resolve(response.body)
+                    if (response.statusCode === 200) return resolve(response.body);
                     else return reject(response)
-
                 })
         });
     };
@@ -40,7 +55,10 @@ export default class Supplier extends Component {
         try {
             this.setState({loading: true});
 
-            const data = await this.reqSupplier(from, to);
+            const data = await this.reqSupplier(dfc(from), dfc(to));
+            data.from = from;
+            data.to = to;
+            data.page = this.state.current;
             dispatch(getSupplierData(data));
 
             this.setState({supplierData: this.props.supplierData.data, loading: false});
@@ -58,18 +76,27 @@ export default class Supplier extends Component {
         }
     };
 
-
     disabledStartDate = (current) => {
         return current && current.valueOf() > Date.now()
     };
 
     render() {
+        let rangeDefaultVal = () => {
+            let from = this.props.supplierData.from !== undefined ? moment(this.props.supplierData.from) : null;
+            let to = this.props.supplierData.to !== undefined ? moment(this.props.supplierData.to) : null;
+
+            return [from, to];
+        };
         return (
             <div style={{padding: 24, background: '#fff', minHeight: 500}}>
                 <RangePicker style={{marginBottom: 10}} disabledDate={this.disabledStartDate} size="large"
-                             onChange={this.onChange}/>
+                             onChange={this.onChangeRangePicker}
+                             defaultValue={rangeDefaultVal()}/>
                 <Spin spinning={this.state.loading}>
-                    <Table rowKey={record => record.id} dataSource={this.state.supplierData} columns={supplierColumns}/>
+                    <Table rowKey={record => record.id} dataSource={this.state.supplierData}
+                           onChange={this.onChangePagination} columns={supplierColumns} pagination={false}/>
+                    <Pagination className="table-pagination-custom" onChange={this.onChangePagination}
+                                current={this.state.current} total={this.props.supplierData.count}/>
                 </Spin>
             </div>
         )
