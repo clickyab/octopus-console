56b5b8d6451c4db7ce40483a78e544bad3e88e92
kasra <kasra.ansari@yahoo.com>
2017-07-18 11:31:56 +0430

    add page title

--- src/app/sciences/layout/index.js
+++ src/app/sciences/layout/index.js
@@ -9,6 +9,7 @@ import {Layout, Breadcrumb, Icon} from 'antd';
 import Supplier from "../supplier/index";
 import Demand from "../demand/index";
 import Exchange from "../exchange/index";
+import DocumentTitle from 'react-document-title';
 import './styles/index.less';
 
 const {Content, Footer, Sider} = Layout;
@@ -46,40 +47,42 @@ export default class ExLayout extends Component {
 
     render() {
         return (
-            <Layout style={{minHeight: this.state.minHeight}}>
-                {this.state.isLogin && <Sider
-                    collapsible
-                    collapsed={this.state.collapsed}
-                    onCollapse={this.onCollapse}
-                >
-                    <Sidebar mode={this.state.mode}/>
-                </Sider>}
-                <Layout>
-                    <Content style={{margin: '0 16px', position: 'relative'}}>
-                        {this.state.isLogin && <Breadcrumb style={{margin: '12px 0'}}>
-                            <Breadcrumb.Item><Link to="/dashboard"><Icon type="home"/></Link></Breadcrumb.Item>
-                            <Breadcrumb.Item>
-                                {this.props.location.pathname !== '/dashboard' ? this.props.location.pathname.replace("/", "") : ''}
-                            </Breadcrumb.Item>
-                        </Breadcrumb>}
-                        <Route exact path="/" render={() => (
-                            this.state.isLogin ? <Redirect to="/dashboard"/> : <Redirect to="/login"/>
-                        )}/>
+            <DocumentTitle title={`Exchange | ${this.props.location.pathname.replace("/", "")} `}>
+                <Layout style={{minHeight: this.state.minHeight}}>
+                    {this.state.isLogin && <Sider
+                        collapsible
+                        collapsed={this.state.collapsed}
+                        onCollapse={this.onCollapse}
+                    >
+                        <Sidebar mode={this.state.mode}/>
+                    </Sider>}
+                    <Layout>
+                        <Content style={{margin: '0 16px', position: 'relative'}}>
+                            {this.state.isLogin && <Breadcrumb style={{margin: '12px 0'}}>
+                                <Breadcrumb.Item><Link to="/dashboard"><Icon type="home"/></Link></Breadcrumb.Item>
+                                <Breadcrumb.Item>
+                                    {this.props.location.pathname !== '/dashboard' ? this.props.location.pathname.replace("/", "") : ''}
+                                </Breadcrumb.Item>
+                            </Breadcrumb>}
+                            <Route exact path="/" render={() => (
+                                this.state.isLogin ? <Redirect to="/dashboard"/> : <Redirect to="/login"/>
+                            )}/>
 
-                        <Route exact path="/login" render={() => (
-                            this.state.isLogin ? <Redirect to="/dashboard"/> : <Login />
-                        )}/>
+                            <Route exact path="/login" render={() => (
+                                this.state.isLogin ? <Redirect to="/dashboard"/> : <Login />
+                            )}/>
 
-                        <ProtectedRoute path="/dashboard" component={Dashboard}/>
-                        <ProtectedRoute path="/supplier" component={Supplier}/>
-                        <ProtectedRoute path="/demand" component={Demand}/>
-                        <ProtectedRoute path="/exchange" component={Exchange}/>
-                    </Content>
-                    <Footer style={{textAlign: 'center'}}>
-                        Clickyab Exchange ©2016 Created by Ant UED
-                    </Footer>
+                            <ProtectedRoute path="/dashboard" component={Dashboard}/>
+                            <ProtectedRoute path="/supplier" component={Supplier}/>
+                            <ProtectedRoute path="/demand" component={Demand}/>
+                            <ProtectedRoute path="/exchange" component={Exchange}/>
+                        </Content>
+                        <Footer style={{textAlign: 'center'}}>
+                            Clickyab Exchange ©2016 Created by Ant UED
+                        </Footer>
+                    </Layout>
                 </Layout>
-            </Layout>
+            </DocumentTitle>
         )
     }
 }
